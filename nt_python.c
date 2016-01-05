d12d015197cb41875c92f19e0705aef776992848
Alex Barghi <abarghi@abarghi.com>
2016-01-05 15:18:10 -0500

    start on python module

---
+++ nt_python.c
@@ -0,0 +1,8 @@
+#include <Python.h>
+#include <number_theory.h>
+static PyObject *numtheory_gcd(PyObject *self, PyObject *args) {
+	int a, b;
+	if(!PyArg_ParseTuple(args, "ii", &a, &b))
+		return NULL;
+	return Py_BuildValue("i", gcd(a, b));
+}
